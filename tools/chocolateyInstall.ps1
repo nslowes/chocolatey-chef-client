38e44b13ef935ea072998a935c6cdac6a2d27ec5
Nick Slowes <nslowes@overdrive.com>
2015-03-08 13:01:14 -0400

    Initial commit

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,9 @@
+﻿$packageName = 'chef-client'
+$installerType = 'msi'
+$url = 'https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/x86_64/chef-windows-11.16.4-1.windows.msi'
+$url64 = $url
+$silentArgs = '/quiet'
+$validExitCodes = @(0)
+
+# installer, will assert administrative rights
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
