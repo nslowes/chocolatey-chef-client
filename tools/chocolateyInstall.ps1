bbdb092a1dcb261be56e397edfb8bbf1e0ffc875
Nick Slowes <nslowes@overdrive.com>
2016-03-18 12:00:18 -0400

    Update for 12.8.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'chef-client'
 $installerType = 'msi'
-$url = 'https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/i386/chef-client-12.5.1-1-x86.msi'
-$url64 = $url
+$url = 'https://packages.chef.io/stable/windows/2008r2/chef-client-12.8.1-1-x86.msi'
+$url64 = 'https://packages.chef.io/stable/windows/2008r2/chef-client-12.8.1-1-x64.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
