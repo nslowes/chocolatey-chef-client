727ed6ce017f83d47b257de2f6ddeadaab82aa60
Nick Slowes <nslowes@overdrive.com>
2016-03-18 11:56:58 -0400

    Update for 12.5.1

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'chef-client'
 $installerType = 'msi'
-$url = 'https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/x86_64/chef-client-12.4.1-1.msi'
+$url = 'https://opscode-omnibus-packages.s3.amazonaws.com/windows/2008r2/i386/chef-client-12.5.1-1-x86.msi'
 $url64 = $url
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
