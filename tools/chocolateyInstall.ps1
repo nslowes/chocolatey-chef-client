37cbffe48df6ebdcd99168e5a1f350ba01d34adb
Nick Slowes <nslowes@overdrive.com>
2016-06-16 17:05:48 -0400

    Updated to version 12.11.18

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'chef-client'
 $installerType = 'msi'
-$url = 'https://packages.chef.io/stable/windows/2008r2/chef-client-12.8.1-1-x86.msi'
-$url64 = 'https://packages.chef.io/stable/windows/2008r2/chef-client-12.8.1-1-x64.msi'
+$url = 'https://packages.chef.io/stable/windows/2008r2/chef-client-12.11.18-1-x86.msi'
+$url64 = 'https://packages.chef.io/stable/windows/2008r2/chef-client-12.11.18-1-x64.msi'
 $silentArgs = '/quiet'
 $validExitCodes = @(0)
 
